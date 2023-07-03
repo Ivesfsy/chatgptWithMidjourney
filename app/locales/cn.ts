c97545a3f6b52e8f9a8b242cbbc0de0ca2bf0ae6
Ives Feng <97079241+Ivesfsy@users.noreply.github.com>
2023-07-03 12:21:09 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "访问密码不正确或为空，请前往[登录](/#/auth)页输入正确的访问密码，或者在[设置](/#/settings)页填入你自己的 OpenAI API Key。",
+      "访问密码不正确或为空，订阅模式请前往[登录](/#/auth)页输入正确的访问密码，积分模式请在[设置](/#/settings)页填入你自己的 MJ API Key。",
   },
   Auth: {
     Title: "需要密码",
