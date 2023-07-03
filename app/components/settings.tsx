fe81f4b2d6e03d346bb2604ac8f91425c7b7c6d3
Ives Feng <97079241+Ivesfsy@users.noreply.github.com>
2023-07-03 12:07:57 +0800

    Update settings.tsx

--- app/components/settings.tsx
+++ app/components/settings.tsx
@@ -372,7 +372,7 @@ export function Settings() {
             {checkingUpdate ? (
               <LoadingIcon />
             ) : hasNewVersion ? (
-              <Link href="https:www.bluelsqkj.com" target="_blank" className="link">
+              <Link href="https://www.bluelsqkj.com" target="_blank" className="link">
                 {Locale.Settings.Update.GoToUpdate}
               </Link>
             ) : (
